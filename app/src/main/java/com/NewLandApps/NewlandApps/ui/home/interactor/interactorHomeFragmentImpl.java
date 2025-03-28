f9a316a9970d4f68c3500ed66b12433f00366335
DIGIMAT <36770624+DIGIMATWEB@users.noreply.github.com>
2025-03-27 19:57:05 -0600

    pending request user 2

--- app/src/main/java/com/NewLandApps/NewlandApps/ui/home/interactor/interactorHomeFragmentImpl.java
+++ app/src/main/java/com/NewLandApps/NewlandApps/ui/home/interactor/interactorHomeFragmentImpl.java
@@ -2,15 +2,20 @@ package com.NewLandApps.NewlandApps.ui.home.interactor;
 
 import android.content.Context;
 
+import com.NewLandApps.NewlandApps.retrofit.LoginServicesV2;
 import com.NewLandApps.NewlandApps.ui.home.presenter.presenterHomeFragment;
 import com.NewLandApps.NewlandApps.ui.home.presenter.presenterHomeFragmentImpl;
+
+import retrofit2.Retrofit;
 
 public class interactorHomeFragmentImpl implements interactorHomeFragment{
     private presenterHomeFragment presenter;
     private Context context;
+    private Retrofit retrofitClient;
+    private LoginServicesV2 service;
     public interactorHomeFragmentImpl(presenterHomeFragment presenter, Context context) {
         this.presenter=presenter;
         this.context=context;
-        
+
     }
 }
